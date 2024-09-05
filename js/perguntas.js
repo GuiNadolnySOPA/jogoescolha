99c2d9905a3eb0acd1fbf0f85295b6c9d56cba9d
Guilherme Nadolny <106606503+GuiNadolnySOPA@users.noreply.github.com>
2024-09-05 19:54:47 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,80 @@
+export const perguntas = [
+    {
+        enunciado: "Você acorda em um QG Militar e um homem aparece no seu campo de visão, ele te explica o ocorrido e te recruta para um passeio",
+        alternativas: [
+            {
+                texto: "Eu preciso ir mesmo?",
+                afirmacao: [
+                    "Você se perguntou se realmente deveria ir àquela expedição.",
+                    "Talvez ter aceitado o convite foi uma má ideia."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Vamos nessa!",
+                afirmacao: [
+                    "Você aceitou ir à expedição sem pensar nas consequências.",
+                    "De fato, deveria pensar mais antes de tomar uma decisão."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao descer de um Jeep Willys 1951, você se depara com um prédio gigante e percebe que está em um grande Hospital, em busca de alguns suprimentos médicos, o grupo que te salvou gastou tudo na sua recuperação.",
+        alternativas: [
+            {
+                texto: "Eu sinto que não é uma boa entrar aí!",
+                afirmacao: [
+                    "Sua intuição estava certa, você não devia ter entrado lá."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "É entrar e sair, né?",
+                afirmacao: [
+                    "Não! Não era só entrar e sair!!"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Entrando no prédio, o silêncio toma conta do lugar, simbolizando que ali está vazio",
+        alternativas: [
+            {
+                texto: "Não podemos baixar a guarda.",
+                afirmacao: [
+                    "Mesmo com a guarda alta, não deu pra segurar."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Mais fácil que tirar doce de criança",
+                afirmacao: [
+                    "Realmente tirar doce de criança era mais fácil."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Entrando em uma sala vazia, um zumbi solitário que ali descansava te morde inesperadamente, e todos sabemos o que resta.",
+        alternativas: [
+            {
+                texto: "SOCORROOOOOOOO!",
+                afirmacao: [
+                    "Seu grito matou seus companheiros também, tudo que dá errado pode piorar."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Eu não acredito nisso...",
+                afirmacao: [
+                    "É difícil acreditar que você morreu de uma forma tão besta."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+]
